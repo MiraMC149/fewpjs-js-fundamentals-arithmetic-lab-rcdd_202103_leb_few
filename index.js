e75c7faaf1c10abae37e516ac835444778faffeb
Mira Chami <miramc149@gmail.com>
2021-04-18 22:24:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 // Write your code here
-let multiply = 2 * 31;
-Math.random(1);
-let mod= num3%num4;
-let max(20,10);
+let num1 = 62;
+let num2 = 1;
+
+let multiply = num1 * num2;
+
+let random = Math.random() * 10
+
+let mod = 20 % 8;
+
+let max = Math.max(1, 2, 20); 
+
